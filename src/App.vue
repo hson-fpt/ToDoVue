de92a5ccd4ad794e0f0a97a7575cff47aa0b6075
Hong Son Ha <zeverse@pop-os.localdomain>
2021-01-10 10:11:44 +0700

    first init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <ToDoTable />
+  </div>
+</template>
+
+<script>
+import ToDoTable from "./components/to-do-table/ToDoTable";
+
+export default {
+  name: "App",
+  components: {
+    ToDoTable,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/to-do-table/ToDoTable.vue
@@ -0,0 +1,10 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+import "ant-design-vue";
+export default {};
+</script>
+
+<style></style>
